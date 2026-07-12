92211535d8c2fd21dcee8914c4eac0c84b107799
dev_x_coder <rmlmanik502@gmail.com>
2026-07-12 21:39:23 +0600

    Created: story data file

---
+++ app/components/story/storyData.ts
@@ -0,0 +1,28 @@
+export const story = [
+  {
+    id: 1,
+    title: "Some people see a car.",
+    subtitle: "Others see freedom.",
+  },
+  {
+    id: 2,
+    title: "Every engine has a heartbeat.",
+    subtitle: "Every driver has a dream.",
+    video: '/videos/weels spping.mp4'
+
+  },
+  {
+    id: 3,
+    title: "Passion isn't measured in horsepower.",
+    subtitle: "It's measured in emotion.",
+    video: '/videos/speedometer.mp4'
+
+  },
+  {
+    id: 4,
+    title: "Machines become legends...",
+    subtitle: "When people fall in love with them.",
+    video:'/videos/story3.mp4'
+
+  },
+];
