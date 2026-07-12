e2808cd2f8053ea95364396cf0d1d09088a90249
dev_x_coder <rmlmanik502@gmail.com>
2026-07-12 21:43:19 +0600

    Created: cars data

---
+++ app/components/cars/carData.ts
@@ -0,0 +1,138 @@
+export const cars = [
+  {
+    id: "bmw",
+    title: "BMW M4 Competition",
+    bgTitle: "M4",
+    quote: "Precision isn't built. It's engineered.",
+
+    stats: [
+      {
+        label: "Horsepower",
+        value: "503 HP",
+      },
+      {
+        label: "0-100",
+        value: "3.4 s",
+      },
+      {
+        label: "Engine",
+        value: "Twin Turbo",
+      },
+      {
+        label: "Drive",
+        value: "xDrive",
+      },
+    ],
+
+    image: "https://i.pinimg.com/1200x/98/35/de/9835de4e0110e4962fe6b29e11fbfe49.jpg",
+
+    glow: "from-cyan-500/40",
+
+    accent: "#00C8FF",
+  },
+
+  {
+    id: "porsche",
+
+    title: "Porsche 911 GT3",
+
+    bgTitle: "911",
+
+    quote: "Born on the track. Perfected for the road.",
+
+    stats: [
+      {
+        label: "Horsepower",
+        value: "502 HP",
+      },
+      {
+        label: "0-100",
+        value: "3.2 s",
+      },
+      {
+        label: "Engine",
+        value: "Flat-6",
+      },
+      {
+        label: "Top Speed",
+        value: "197 MPH",
+      },
+    ],
+
+    image: "https://i.pinimg.com/736x/07/c4/96/07c496cf53714188a1557b1b731206ee.jpg",
+
+    glow: "from-orange-500/40",
+
+    accent: "#ff8c00",
+  },
+
+  {
+    id: "ferrari",
+
+    title: "Ferrari F8 Tributo",
+
+    bgTitle: "F8",
+
+    quote: "Beauty becomes speed.",
+
+    stats: [
+      {
+        label: "Horsepower",
+        value: "710 HP",
+      },
+      {
+        label: "0-100",
+        value: "2.9 s",
+      },
+      {
+        label: "Engine",
+        value: "V8 Twin Turbo",
+      },
+      {
+        label: "Top Speed",
+        value: "211 MPH",
+      },
+    ],
+
+    image: "https://i.pinimg.com/1200x/2e/9e/af/2e9eafbcd62b02a9fddf5183f8c236f6.jpg",
+
+    glow: "from-red-500/40",
+
+    accent: "#ff3d3d",
+  },
+
+  {
+    id: "gtr",
+
+    title: "Nissan GT-R",
+
+    bgTitle: "GT-R",
+
+    quote: "Legends never retire.",
+
+    stats: [
+      {
+        label: "Horsepower",
+        value: "565 HP",
+      },
+      {
+        label: "0-100",
+        value: "2.9 s",
+      },
+      {
+        label: "Engine",
+        value: "VR38DETT",
+      },
+      {
+        label: "AWD",
+        value: "ATTESA",
+      },
+    ],
+
+    image: "https://i.pinimg.com/1200x/c9/2d/df/c92ddfbcca104f1177d4855cd146652a.jpg",
+
+    glow: "from-violet-500/40",
+
+    accent: "#7c3aed",
+  },
+];
